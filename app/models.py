18dde359bd04e0dce50ea18c15879cac281ccabf
王友余 <xia0sheng@wangyouyus-MacBook-Pro.local>
2018-11-01 11:41:24 +0800

    mac 1

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    email = db.Column(db.String64, unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
--- flasky.py
+++ flasky.py
@@ -10,3 +10,7 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
+
+
+if __name__ == '__main__':
+    app.run()
